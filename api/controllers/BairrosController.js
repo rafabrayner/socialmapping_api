66985b3c80daf69e4eed247d3e52ca6d627b6f15
Rafael <rafabrayner92@gmail.com>
2018-08-18 14:31:08 -0300

    Cria api saisljs

---
+++ api/controllers/BairrosController.js
@@ -0,0 +1,12 @@
+/**
+ * BairrosController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  
+
+};
+
